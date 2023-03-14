0f593d165dedbc656da61455a8bfd24ca3c8d907
maguoqun <guoqun.ma@spacemit.com>
2023-03-14 20:52:19 +0800

    add package blogbench
    Change-Id: I71bd20356803af4568118b3c35d25f052fa16bc3

---
+++ package/blogbench/blogbench.mk
@@ -0,0 +1,25 @@
+################################################################################
+#
+# blogbench
+#
+################################################################################
+
+BLOGBENCH_VERSION = 3f28ad53d087b850719877d0b9c1631f99a16e31
+BLOGBENCH_SITE = https://github.com/jedisct1/Blogbench.git
+BLOGBENCH_SITE_METHOD=git
+BLOGBENCH_LICENSE = BSD 2-Clause and MIT
+#BLOGBENCH_LICENSE_FILES = LICENSE
+BLOGBENCH_AUTORECONF = YES
+
+define POSTMARK_BUILD_CMDS
+	$(TARGET_MAKE_ENV) ./aclocal
+	$(TARGET_MAKE_ENV) ./autoheader
+	$(TARGET_MAKE_ENV) ./autoconf
+
+endef
+
+define BLOGBENCH_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/src/blogbench $(TARGET_DIR)/usr/bin/blogbench
+endef
+
+$(eval $(autotools-package))
