179fa07d840aaa3cf21476034fbaa2768e723c77
maguoqun <guoqun.ma@spacemit.com>
2023-03-21 20:40:12 +0800

    tachyon: optz compile cmd
    Change-Id: Ia4f35295226f4bec6103dae6cab73d2a45e1d257

--- package/tachyon/tachyon.mk
+++ package/tachyon/tachyon.mk
@@ -6,30 +6,20 @@
 
 TACHYON_VERSION = 4e347495c3d996ed5f68d96a424d481d030843da
 TACHYON_SITE = https://gitee.com/spacemit/Tachyon.git
-TACHYON_SITE_METHOD=git
+TACHYON_SITE_METHOD = git
 TACHYON_INSTALL_TARGET = YES
-#CPPFLAGS=$(TARGET_CPPFLAGS) -xc++ -lstdc++ -shared-libgcc
 
-CFLAGS += -Wall -O3 -fomit-frame-pointer -ffast-math -DLinux
-
-linux-64-flag = "ARCH = linux-64" \
-	"AR = ar" \
-	"ARFLAGS = r" \
-	"RANLIB = ranlib" \
-	"LIBS = -L. -ltachyon $(MISCLIB) -lm"
+TACHYON_CFLAGS = $(TARGET_CFLAGS) -Wall -ffast-math -DLinux
 
 define TACHYON_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE1) CC="$(TARGET_CC) $(CFLAGS) $(MISCFLAGS) $(TARGET_LDFLAGS)" \
-		-C $(@D)/unix all $(linux-64-flag)
+	cp -f package/tachyon/Make-arch $(@D)/unix/
+	(cd $(@D)/unix; $(TARGET_MAKE_ENV) $(MAKE1) CC=$(TARGET_CC) linux-riscv64 ;)
 
 endef
 
-define TACHYON_INSTALL_STAGING_CMDS
-
-endef
 
 define TACHYON_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/compile/linux-64/tachyon $(TARGET_DIR)/usr/bin
+	$(INSTALL) -D -m 0755 $(@D)/compile/linux-riscv64/tachyon $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
