417219527d5b69588a74ece36fa329ca1488df88
max <guoqun.ma@spacemit.com>
2024-01-18 22:10:13 +0800

    libopenssl: add k1 afalg engine support
    Change-Id: I64626ac95e09fb6735305b489400f108c449332e

--- package/libopenssl/libopenssl.mk
+++ package/libopenssl/libopenssl.mk
@@ -82,7 +82,6 @@ define LIBOPENSSL_CONFIGURE_CMDS
 			no-tests \
 			no-fuzz-libfuzzer \
 			no-fuzz-afl \
-			no-afalgeng \
 			$(if $(BR2_PACKAGE_LIBOPENSSL_ENABLE_CHACHA),,no-chacha) \
 			$(if $(BR2_PACKAGE_LIBOPENSSL_ENABLE_RC2),,no-rc2) \
 			$(if $(BR2_PACKAGE_LIBOPENSSL_ENABLE_RC4),,no-rc4) \
